d1b4196c7a206fcd0cf85fb842dc0ce973d8ce73
Dan Peterjohn <drp7.acc@gmail.com>
2020-04-18 18:50:22 -0500

    Barrels explode other barrels. Explosions have delays.

--- Assets/Scripts/ObjectController.cs
+++ Assets/Scripts/ObjectController.cs
@@ -5,8 +5,8 @@ using UnityEngine.Assertions;
 
 namespace PBJ
 {
-	public class ObjectController : MonoBehaviour
-	{
+    public class ObjectController : MonoBehaviour
+    {
         [SerializeField]
         private float m_objectHeight;
         public float ObjectHeight
@@ -16,107 +16,125 @@ namespace PBJ
                 return m_objectHeight;
             }
         }
-		[SerializeField]
-		private bool m_isExplosive;
-		private bool m_isBreakable;
+		
+        [Header("Explosion Attributes")]
+        [SerializeField]
+        private bool m_isExplosive;
+        [SerializeField]
+        private float m_isExplosionDelaySec;
+        [SerializeField]
+        private GameObject m_explosionPrefab;
 
-		public enum ObjectState
-		{
-			Breaking = 1,
-			Exploding = 2
-		}
+        [Space]
+        [SerializeField]
+        private bool m_isBreakable;
 
-		private ObjectState m_objectState;
+        public enum ObjectState
+        {
+            Breaking = 1,
+            Exploding = 2
+        }
 
-		[SerializeField]
-		private GameObject m_explosionPrefab;
+        private ObjectState m_objectState;
 
-		private Rigidbody2D m_rigid;
+        private Rigidbody2D m_rigid;
         private Collider2D m_collider;
 
-		private void Awake()
-		{
+        private void Awake()
+        {
 			if (!TryGetComponent<Rigidbody2D>(out m_rigid))
-			{
-				Debug.LogError("No rigidbody found");
-			}
-            if(!TryGetComponent<Collider2D>(out m_collider))
+            {
+                Debug.LogError("No rigidbody found");
+            }
+            if (!TryGetComponent<Collider2D>(out m_collider))
             {
                 Debug.LogError("No collider found");
             }
-		}
-		public void Explode()
-		{
-			Assert.IsTrue(m_isExplosive);
-			Assert.IsNotNull(m_explosionPrefab);
+        }
 
-			m_objectState = ObjectState.Exploding;
+        private IEnumerator ExplodeRoutine()
+        {
+            yield return new WaitForSeconds(m_isExplosionDelaySec);
 
-			Instantiate(m_explosionPrefab, transform.position, transform.rotation);
-			gameObject.SetActive(false);
-		}
+            m_objectState = ObjectState.Exploding;
 
-		public bool IsExplosive()
-		{
-			return m_isExplosive;
-		}
+            Instantiate(m_explosionPrefab, transform.position, transform.rotation);
+            gameObject.SetActive(false);
+        }
 
-		private bool CanExplode()
-		{
-			return m_isExplosive && m_objectState != ObjectState.Exploding && m_objectState != ObjectState.Breaking;
-		}
 
-		public void Break()
-		{
-			Assert.IsTrue(m_isBreakable);
+        public void Explode()
+        {
+            Assert.IsTrue(m_isExplosive);
+            Assert.IsNotNull(m_explosionPrefab);
 
-			m_objectState = ObjectState.Breaking;
+            if (!CanExplode())
+                return;
 
-			if (m_isExplosive)
-			{
-				Explode();
-			}
-			else
-			{
-				// Break routine???
-			}
-		}
+            m_objectState = ObjectState.Exploding;
+            StartCoroutine(ExplodeRoutine());
+        }
 
-		public bool IsBreakable()
-		{
-			return m_isBreakable;
-		}
+        public bool IsExplosive()
+        {
+            return m_isExplosive;
+        }
 
-		private bool CanBreak()
-		{
-			return m_isBreakable && m_objectState != ObjectState.Exploding && m_objectState != ObjectState.Breaking;
-		}
+        private bool CanExplode()
+        {
+            return m_isExplosive && m_objectState != ObjectState.Exploding && m_objectState != ObjectState.Breaking;
+        }
 
-		private void OnCollisionEnter2D(Collision2D collision)
-		{
-			if (collision.gameObject.tag == "Explosion")
-			{
-				if (CanExplode())
-					Explode();
-				else if (CanBreak())
-					Break();
-			}
-		}
+        public void Break()
+        {
+            Assert.IsTrue(m_isBreakable);
 
-		public void PickedUp()
-		{
+            m_objectState = ObjectState.Breaking;
+
+            if (m_isExplosive)
+            {
+                Explode();
+            }
+            else
+            {
+                // Break routine???
+            }
+        }
+
+        public bool IsBreakable()
+        {
+            return m_isBreakable;
+        }
+
+        private bool CanBreak()
+        {
+            return m_isBreakable && m_objectState != ObjectState.Exploding && m_objectState != ObjectState.Breaking;
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.tag == "Explosion")
+            {
+                if (CanExplode())
+                    Explode();
+                else if (CanBreak())
+                    Break();
+            }
+        }
+        public void PickedUp()
+        {
             m_rigid.bodyType = RigidbodyType2D.Kinematic;
             m_rigid.velocity = Vector3.zero;
             m_collider.enabled = false;
-		}
+        }
 
 		public void Throw(Vector2 origin, Vector2 force)
-		{
+        {
             transform.SetParent(null);
             transform.position = origin;
             m_rigid.bodyType = RigidbodyType2D.Dynamic;
             m_collider.enabled = true;
             m_rigid.AddForce(force, ForceMode2D.Impulse);
-		}
-	}
+        }
+    }
 }
