0d4c64a054719066966fbee00cfa4afc4a44022a
Raymond John Grice <raymondjgrice@gmail.com>
2020-04-18 16:31:49 -0700

    Added ability to disable actions

--- Assets/Scripts/PlayerStatus.cs
+++ Assets/Scripts/PlayerStatus.cs
@@ -14,5 +14,19 @@ namespace PBJ
         public float PickupSpeed;
         public AnimationCurve PickupSpeedCurve;
         public LayerMask ItemMask;
+
+        public bool CanAct
+        {
+            get
+            {
+                return m_canAct;
+            }
+        }
+        private bool m_canAct = true;
+
+        public void SetCanAct(bool act)
+        {
+            m_canAct = act;
+        }
 	}
 }
