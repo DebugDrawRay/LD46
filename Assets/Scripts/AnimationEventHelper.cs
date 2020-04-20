88bc5324c74b856d1858ebad395a55ead274e5bc
Raymond John Grice <raymondjgrice@gmail.com>
2020-04-19 20:25:28 -0700

    Added footsteps

---
+++ Assets/Scripts/AnimationEventHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace PBJ
+{
+	public class AnimationEventHelper : MonoBehaviour
+	{
+        [SerializeField]
+        private UnityEvent m_event;
+
+        public void TriggerEvent()
+        {
+            m_event.Invoke();
+        } 
+	}
+}
